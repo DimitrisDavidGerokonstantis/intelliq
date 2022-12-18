495a505be9449a0d70d88f54367f8b6da1cf3bf2
ntua-el19009 <115417360+ntua-el19009@users.noreply.github.com>
2022-12-18 10:50:45 +0200

    Preliminary version of our frontend
    Includes an index.js file that contains the main core of our frontend, an App.js file (with the questionnaire) and a Finished.js file (ui after finishing answering the questionnaire). For now we are using dummy data from another Javascript file.

---
+++ frontend/Finished.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Finished(){
+  return(
+    <div>
+    <h1 className = "end">Finished!</h1>
+    <h2>See a summary of your answers by clicking here: dummylink</h2>
+    </div>
+  )
+}
+
+
+export default Finished
