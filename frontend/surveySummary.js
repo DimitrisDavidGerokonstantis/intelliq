8f1c3a600586086f4c3e5ad35b217b050159ef24
ntua-el19209 <115417433+ntua-el19209@users.noreply.github.com>
2023-01-10 18:58:21 +0200

    Added surveySummary.js
    surveySummary.js constructs the summary of the last questionnaire added into the database and includes the ability for the flow to be determined.

---
+++ frontend/surveySummary.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:5000/getSurveysSummary')
+    .then(response => response.json())
+    .then(data => loadHTMLTable(data['data']));
+    
+});
+
+var flow_counter;
+var flow_array = [];
+var help_array = [];
+function loadHTMLTable(data) {
+    const div = document.querySelector('#div_summary');
+    flow_counter = 0;
+    let tableHtml = "";
+    let help_title="";
+    var counter ;
+    data.forEach(function ({qtitle, anstitle,required, ansid, qid}) {       
+        if(help_title!=qtitle){
+            tableHtml += `<h2>(#${qid}) ${qtitle}`;
+            if(required === 1)tableHtml += ` *`;
+            counter = 0;
+            tableHtml +=`</h2>`;
+ 
+        }
+        counter +=1;
+        tableHtml += `<h3>${counter}. ${anstitle} `;
+        tableHtml += `<input type="text" placeholder="Flow" id="flow_id${flow_counter + 1}">`;
+        help_array.push(ansid);
+        flow_counter += 1;
+        tableHtml += `</h3>`;
+        help_title = qtitle;
+    });
+
+    div.innerHTML = tableHtml;
+}
+
+const finishBtn = document.querySelector('#finish-btn');
+finishBtn.onclick = function () {
+    var help2;
+    for(var i = 0; i < flow_counter; i++){
+        help2 = i+1;
+        flowInput = document.querySelector('#'+'flow_id'+ help2);
+        //console.log('answerInput');console.log(answerInput);
+        flow_array.push(flowInput.value);
+        flowInput.value = "";
+    }
+    
+    fetch('http://localhost:5000/updateFlow', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'PATCH',
+        body: JSON.stringify({ flow_array : flow_array, help_array : help_array, flow_counter : flow_counter })
+    })
+    .then(response => response.json());
+    location.replace('index.html');
+   // .then(data => insertRowIntoTable(data['data']));
+}
