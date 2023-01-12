fb76eaec5cdbebfd883c77f067d69baf3dbfc01d
ntua-el19209 <115417433+ntua-el19209@users.noreply.github.com>
2023-01-12 20:40:26 +0200

    Updated showQuestionnaires.js
    Added functionality in order to support the answers' submission

--- frontend/showQuestionnaires.js
+++ frontend/showQuestionnaires.js
@@ -21,7 +21,7 @@ function loadHTMLTable(data) {
         tableHtml += `<td>${id}</td>`;
         tableHtml += `<td>${title}</td>`;
         tableHtml += `<td>${keywords}</td>`;
-        tableHtml += `<td><button id="${help_counter}" onclick="myFunction(${id})" value="${id}">Answer</button></td>`;
+        tableHtml += `<td><button id="${help_counter}" onclick="createSession(${id})" value="${id}">Answer</button></td>`;
         tableHtml += "</tr>";
         help_counter +=1;
         console.log(help_counter);
@@ -31,17 +31,108 @@ function loadHTMLTable(data) {
 }
 
 
-function myFunction(button_value) {
-    console.log('hello');
-    fetch('http://localhost:5000/save_session', {
+function loadHTMLTable2(data) {
+    const main = document.querySelector('#showSurveys_main');
+    let tableHtml = "";
+    let help_title="";
+    let last = true;
+    var sessionID ;
+    tableHtml += `<h1>Answer that Question<h1>`;
+    data.forEach(function ({quesid, questitle,atitle, nextque, answerid, surid, sessID}) {       
+           if(help_title!=questitle) tableHtml += `<h2>Question : ${questitle}</h2>`;
+            tableHtml += "<h3>";
+            tableHtml += `<input type="radio" class="form-control" id="${answerid}" name="${surid}" value="${quesid}">`;
+            tableHtml +=`<label for="html"> ${atitle}</label><br></br></h3>`
+            help_title = questitle;
+            if(nextque!=0)last=false;
+            sessionID = sessID;
+
+    });
+    if(last!=true){
+        tableHtml +=`<div class='button'>`;
+        tableHtml += `<button id="next-question-btn" onclick="next_question('${sessionID}')">Next</button>`;
+        tableHtml +=`</div>`;}
+    else {
+        tableHtml +=`<div class='button'>`;
+        tableHtml += `<button id="end-answer-btn" onclick="end('${sessionID}')">End</button>`;
+        tableHtml +=`</div>`;}
+
+    main.innerHTML = tableHtml;
+}
+
+
+function end(sessionID) {
+    var ele = document.getElementsByClassName('form-control');
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked){
+                    
+                    fetch('http://localhost:5000/doanswer/'+ele[i].name+'/'+ele[i].value+'/'+ sessionID +'/'+ele[i].id, {
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    method: 'POST'
+                    })
+                    .then(response => response.json());
+                    
+                }
+                    
+            }
+    location.replace('index.html');
+}
+function next_question(sessionID) {
+    var ele = document.getElementsByClassName('form-control');
+    console.log('ele',ele);
+            for(i = 0; i < ele.length; i++) {
+                if(ele[i].checked){
+                    
+                    let option = ele[i].id;
+                    fetch('http://localhost:5000/doanswer/'+(ele[i].name)+'/'+(ele[i].value)+'/'+ sessionID +'/'+(ele[i].id), {
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    method: 'POST'
+                    })
+                    .then(response => response.json());
+                    console.log('frontentd sessionID',sessionID);
+                    myFunction2(option,sessionID);
+                }
+            }
+}
+
+function createSession(surveyID) {
+    fetch('http://localhost:5000/create_session/'+surveyID, {
         headers: {
             'Content-type': 'application/json'
         },
-        method: 'POST',
-        body: JSON.stringify({ button_value:button_value})
+        method: 'POST'
     })
-    .then(response => response.json())
-    location.replace('answerSurvey.html');
+    .then(response => response.json()).then(data => GetFirstQuestion(data['data']));
+}
+function GetFirstQuestion(Data) {
+    console.log('Data');console.log(Data);
+    let sesID = Data.session_id;
+    let survID = Data.survID;
+
+    fetch('http://localhost:5000/answer_survey/'+sesID+'/'+survID, {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'GET'
+    })
+    .then(response => response.json()).then(document.getElementById('showSurveys_main').innerHTML = "")
+    .then(data => loadHTMLTable2(data['data']));
+}
+
+function myFunction2(option,sessionID) {
+    fetch('http://localhost:5000/next_question/'+option+'/'+sessionID, {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'GET'
+    })
+    .then(response => response.json()).then(document.getElementById('showSurveys_main').innerHTML = "")
+    .then(data => loadHTMLTable2(data['data']));
+
 }
 
 /*const chooseBtn = document.querySelector('#choose-survey-btn');
