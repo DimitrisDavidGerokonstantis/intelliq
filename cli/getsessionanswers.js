b365d306b9a60d37866b3b817827a90a30369d31
ntua-el19209 <el19209@mail.ntua.gr>
2023-01-21 14:45:09 +0200

    Modified cli folder in order to support csv format
    Added support for csv format in CLI

--- cli/getsessionanswers.js
+++ cli/getsessionanswers.js
@@ -12,13 +12,15 @@ yargs.command(
 
  yargs.positional('questionnaire_id', { describe: 'Identifier of the questionnaire'});
  yargs.positional('session_id', { describe: 'Identifier of the session'});      
+ yargs.positional('format', { describe: 'Define the format of the result (json or csv)'});   
 
 let argv = yargs.argv;
 let questionnaire_id = argv.questionnaire_id;
 let session_id = argv.session_id;
+let format = argv.format;
 
 
-if(((process.argv.length - 2)/2) != 2){
+if(((process.argv.length - 2)/2) != 3){
   console.log('Some arguments missing. Use --help argument to check the documentation of the command');
 
 }
@@ -31,7 +33,23 @@ axios.get(url,{
 })
 .then((response) => {
     let answers = JSON.stringify(response.data).slice(9,-2);
-    console.log(answers);
+    const csvString = [
+        [
+          "Question_Title",
+          "Questionnaire_ID",
+          "Answer_Title"
+        ],
+        ...(response.data.data).map(item => [
+          item.quetitle,
+          item.quesid,
+          item.anstitle
+        ])
+      ]
+       .map(e => e.join(",")) 
+       .join("\n");
+    if(format=='json')console.log(answers);
+    if(format=='csv')console.log(csvString);
+    if(format!='json' && format!='csv') console.log('Uknown Format');
 })
 .catch(err => {
     console.log(err);
