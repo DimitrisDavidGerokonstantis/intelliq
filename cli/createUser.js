83b32f50bf0034d2e03dce0329d62258b58d0750
ntua-el19209 <el19209@mail.ntua.gr>
2023-02-14 21:34:38 +0200

    Added createUser.js
    Added CLI command 'createUser' in order to add a new user

---
+++ cli/createUser.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+const yargs = require('yargs');
+const axios = require('axios').default;
+
+// Define the name and the arguments of the command
+yargs.command(
+    'createUser', 
+    'Create a new User', // Command description for --help  
+    );
+
+yargs.positional('username', { describe: 'Username to be created'});
+yargs.positional('password', { describe: 'Password to be created'}); 
+
+
+//read the arguments
+let argv = yargs.argv;
+let username = argv.username;
+let password = argv.password;
+
+// if some arguments are missing
+if(((process.argv.length - 2)/2) != 2){       
+    console.log('Some arguments missing. Use --help argument to check the documentation of the command'); 
+}
+
+// call the required API route in order for the command to do what it is supposed to do
+else{
+    let url = `http://localhost:5000/admin/createUser`;
+    axios.post(url, {
+        username:username,
+        password:password
+})
+    .then((response) => { // if everything is alright
+        process.exitCode = response.status;
+        console.log('User Added')})
+    
+    .catch((error)=> {  // if an error occurs
+        console.log('Error on adding the new user');
+    })
+}
